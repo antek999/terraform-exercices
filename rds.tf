fd0e5c72b36c2dc10975aeea27ab32b975377682
antek999 <bartoszburski7@gmail.com>
2020-11-29 20:46:10 +0100

    test

--- rds.tf
+++ rds.tf
@@ -8,7 +8,7 @@ resource "aws_db_subnet_group" "db-subnets" {
 }
 
 resource "aws_db_instance" "main-db" {
-  allocated_storage      = 20
+  allocated_storage                   = 20
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "5.7"
