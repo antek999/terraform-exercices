200e3d969dab349c964e1083a1718395d294f40b
antek999 <bartoszburski7@gmail.com>
2020-11-29 14:33:50 +0100

    add github actions workflow with terraform fmt

--- sg-pub.tf
+++ sg-pub.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "sg-ssh-pub" {
   }
 
   ingress {
-    description = "ssh"
+    description =                                     "ssh"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
